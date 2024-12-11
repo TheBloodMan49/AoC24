5c970b03cb9fd9b8b6968d69910f5449971a5e57
Paul <79766427+TheBloodMan49@users.noreply.github.com>
2024-12-11 09:23:46 +0100

    Day 10 working

---
+++ 11/solve_1.py
@@ -0,0 +1,25 @@
+def iteration(data: list[int]) -> list[int]:
+    new_data = []
+    for elem in data:
+        if elem == 0:
+            new_data.append(1)
+        elif len(str(elem)) % 2 == 0:
+            string = str(elem)
+            first = string[:len(string)//2]
+            second = string[len(string)//2:]
+            new_data.append(int(first))
+            new_data.append(int(second))
+        else:
+            new_data.append(elem*2024)
+    return new_data
+
+def main(file: str):
+    with open(file) as f:
+        data = map(int,f.read().strip().split(" "))
+
+        for _ in range(25):
+            data = iteration(data)
+
+        print(len(data))
+
+main("input.txt")
---
+++ 11/solve_2.py
@@ -0,0 +1,36 @@
+from functools import lru_cache
+from collections import defaultdict
+from math import log
+
+def iteration(data: defaultdict) -> defaultdict:
+    new_data = defaultdict(int)
+    for elem, count in data.items():
+        for new_elem in process_element(elem):
+            new_data[new_elem] += count
+    return new_data
+
+@lru_cache(maxsize=None)
+def process_element(elem: int) -> list[int]:
+    if elem == 0:
+        return [1]
+    elif int(log(elem,10) + 1) % 2 == 0:
+        length = int(log(elem,10) + 1)
+        divisor = 10 ** (length // 2)
+        first = elem // divisor
+        second = elem % divisor
+        return [first, second]
+    else:
+        return [elem * 2024]
+
+def main(file: str):
+    with open(file) as f:
+        data = list(map(int, f.read().strip().split(" ")))
+        data_dict = defaultdict(int, {elem: 1 for elem in data})
+
+        for _ in range(75):
+            data_dict = iteration(data_dict)
+
+        #print(len(data_dict))
+        print(sum(data_dict.values()))
+
+main("input.txt")
